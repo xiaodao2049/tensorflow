56aa10af432c8a827fb744333fec69cedf39208d
xiaodao2049 <15008270267@163.com>
2018-05-13 15:17:07 +0800

    5.13

---
+++ tf5_example2/for_you_to_practice.py
@@ -0,0 +1,21 @@
+ 
+
+"""
+Please note, this code is only for python 3+. If you are using python 2+, please modify the code accordingly.
+"""
+from __future__ import print_function
+import tensorflow as tf
+import numpy as np
+
+# create data
+x_data = np.random.rand(100).astype(np.float32)
+y_data = x_data*0.1 + 0.3
+
+### create tensorflow structure start ###
+Weights=tf.Variable(tf.random_uniform([1]),-1,1)
+### create tensorflow structure end ###
+          # Very important
+
+for step in range(201):
+    pass
+
